ab3b3c9e6b1d03899446a188b537d63dc8606eca
furkanarslan <furkan.arslan@doruksistem.com.tr>
2025-04-02 17:33:26 +0300

    redundant code

--- app/dashboard/documents/page.tsx
+++ app/dashboard/documents/page.tsx
@@ -1,17 +1,11 @@
 import { DocumentHeader } from "@/components/dashboard/documents/document-header";
 import { DocumentList } from "@/components/dashboard/documents/document-list";
-import { NextIntlClientProvider } from "next-intl";
-import { getTranslations } from "@/app/i18n/i18n";
 
-export default async function DocumentsPage() {
-  const messages = await getTranslations('tr');
-  
+export default function DocumentsPage() {
   return (
-    <NextIntlClientProvider messages={messages} locale="tr">
-      <div className="space-y-6">
-        <DocumentHeader />
-        <DocumentList />
-      </div>
-    </NextIntlClientProvider>
+    <div className="space-y-6">
+      <DocumentHeader />
+      <DocumentList />
+    </div>
   );
 }
